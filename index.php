8d8eca955651dda96ac628969765857540f78580
Vinícius Alcântara <vinicius.redes2011@gmail.com>
2018-12-10 17:00:09 -0300

    Página Inicial Concluída

---
+++ index.php
@@ -0,0 +1,360 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Orlando City</title>
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/orlando.css">
+        <link rel="stylesheet" href="fontawesome/web-fonts-with-css/css/fontawesome-all.min.css">
+        <link rel="stylesheet" type="text/css" href="hover/hover-min.css">
+    </head>
+<body>
+    <!-- Cabeçalho -->
+    <header>
+        <div class="header-black">
+            <div class="container">
+                <div class="col-xs-12 col-md-12 col-lg-12 ">
+                    <nav>
+                        <ul class="nav navbar-nav center-block">
+                            <li><a href="#"><img class="brasoes img-responsive" src="img/clubs/america-mg.png"></a></li>
+                            <li><a href="#"><img class="brasoes img-responsive" src="img/clubs/atletico-mg.png"></a></li>
+                            <li><a href="#"><img class="brasoes img-responsive" src="img/clubs/atletico-pr.png"></a></li>
+                            <li><a href="#"><img class="brasoes img-responsive" src="img/clubs/bahia.png"></a></li>
+                            <li><a href="#"><img class="brasoes img-responsive" src="img/clubs/botafogo.png"></a></li>
+                            <li><a href="#"><img class="brasoes img-responsive" src="img/clubs/ceara.png"></a></li>
+                            <li><a href="#"><img class="brasoes img-responsive" src="img/clubs/chapecoense.png"></a></li>
+                            <li><a href="#"><img class="brasoes img-responsive" src="img/clubs/corinthians.png"></a></li>
+                            <li><a href="#"><img class="brasoes img-responsive" src="img/clubs/cruzeiro.png"></a></li>
+                            <li><a href="#"><img class="brasoes img-responsive" src="img/clubs/flamengo.png"></a></li>
+                            <li><a href="#"><img class="brasoes img-responsive" src="img/clubs/fluminense.png"></a></li>
+                            <li><a href="#"><img class="brasoes img-responsive" src="img/clubs/gremio.png"></a></li>
+                            <li><a href="#"><img class="brasoes img-responsive" src="img/clubs/internacional.png"></a></li>
+                            <li><a href="#"><img class="brasoes img-responsive" src="img/clubs/palmeiras.png"></a></li>
+                            <li><a href="#"><img class="brasoes img-responsive" src="img/clubs/parana-clube.png"></a></li>
+                            <li><a href="#"><img class="brasoes img-responsive" src="img/clubs/santos.png"></a></li>
+                            <li><a href="#"><img class="brasoes img-responsive" src="img/clubs/sao-paulo.png"></a></li>
+                            <li><a href="#"><img class="brasoes img-responsive" src="img/clubs/sport.png"></a></li>
+                            <li><a href="#"><img class="brasoes img-responsive" src="img/clubs/vasco.png"></a></li>
+                            <li><a href="#"><img class="brasoes img-responsive" src="img/clubs/vitoria.png"></a></li>
+                        </ul>
+                    </nav>
+            </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                <nav id="menu" class="pull-right">
+                    <ul>
+                        <li><a href="#">Tickets</a></li>
+                        <li><a href="#">News</a></li>
+                        <li><a href="#">Schedule</a></li>
+                        <li class="search">
+                            <div class="input-group">
+                                <input type="search" class="form-control" placeholder="Search...">
+                                <span class="input-group-btn">
+                                    <button class="btn btn-default" type="button"><i class="fa fa-search"></i></button>
+                                </span>
+                            </div>
+                        </li>
+                    </ul>
+                </nav>
+                </div>
+            </div>
+                <div id="caixa-logo" class="container">
+                    <img id="logomarca" src="img/logo-orlando.png" alt="logomarca">
+                </div>
+        </div>
+    </header> <!-- Fim do Cabçalho -->
+<!-- Carousel -->
+<div id="carousel-img-orlando" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-img-orlando" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-img-orlando" data-slide-to="1"></li>
+    <li data-target="#carousel-img-orlando" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+        <img src="img/banners/estadio.png" alt="Estadio">
+      <div class="carousel-caption banners-text-img">
+          <h1>Novo Orlando City Soccer Stadium é aberto ao público</h1>
+      </div>
+    </div>   
+    <div class="item">
+        <img src="img/banners/kaka.png" alt="Kaka">
+        <div class="carousel-caption banners-text-img">
+          <h1>Kaká é o mais novo contratado do Orlando City</h1>
+      </div>
+    </div>
+    <div class="item">
+        <img src="img/banners/jogo.png" alt="Jogo">
+      <div class="carousel-caption banners-text-img">
+          <h1>Acompanhe nossos jogos nos canais ESPN</h1>
+      </div>
+    </div>
+  </div>
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-img-orlando" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-img-orlando" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div><!-- Fim do Carousel -->
+<!-- Notícias -->
+<section id="news">
+    <div class="container">
+        <div class="row">
+            <div id="noticias" class="col-md-4">
+                <h2>Latest News</h2>
+                <hr>
+                <a href="#">
+                <div class="row">
+                    <div id="noticia01" class="col-md-6 notice-img hvr-grow">
+                    </div>
+                    <div class="col-md-6">
+                        <article>
+                        <p class="text-justify">What Marta Brings To The Orlando Pride | Orlando City Soccer Club</p><time>May 16, 2018</time>
+                        </article>
+                    </div>
+                </div>
+                    <hr/>
+                </a>
+                <a href="#">
+                <div class="row">
+                <div id="noticia02" class="col-md-6 notice-img hvr-grow">
+                    </div>
+                    <div class="col-md-6">
+                        <article>
+                        <p class="text-justify">Long Road Trip Gives Pride Momentum Going Forward</p><time>May 15, 2018</time>
+                        </article>
+                    </div>
+                </div>
+                    <hr/>
+                </a>
+                <a href="#">
+                <div class="row">
+                    <div id="noticia03" class="col-md-6 notice-img hvr-grow">
+                    </div>
+                    <div class="col-md-6">
+                        <article>
+                        <p class="text-justify">Traveling Supporters' the guide Toronto</p><time>May 15, 2018</time>
+                        </article>
+                    </div>
+                </div>
+                    <hr/>
+                </a>
+                <a href="#">
+                <div class="row">
+                <div id="noticia04" class="col-md-6 notice-img hvr-grow">
+                    </div>
+                    <div class="col-md-6">
+                        <article>
+                        <p class="text-justify">By The Numbers: Headed to the Six</p><time>May 15, 2018</time>
+                        </article>
+                    </div>
+                </div>
+                    <hr/>
+                </a>
+                <a href="#">
+                <div class="row">
+                    <div id="noticia05" class="col-md-6 notice-img hvr-grow">
+                    </div>
+                    <div class="col-md-6">
+                        <article>
+                        <p class="text-justify">.GIF | This and That</p><time>May 15, 2018</time>
+                        </article>
+                    </div>
+                </div>
+                    <hr/>
+                </a>
+                <a href="#">
+                <div class="row">
+                <div id="noticia06" class="col-md-6 notice-img hvr-grow">
+                    </div>
+                    <div class="col-md-6">
+                        <article>
+                        <p class="text-justify">SoccerCast live in Conflict Analysis</p><time>May 15, 2018</time>
+                        </article>
+                    </div>
+                </div>
+                    <hr/>
+                </a>
+                </div>
+            <div id="videos" class="col-md-4">
+                <h2>Latest Videos</h2>
+                <hr>
+                <div class="row">
+                    <div class="col-md-12">
+                        <h4 class="text-justify">Hear comments from Marta ahead of Orlando Pride's match vs. the Boston Breakers</h4>
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/tfwi8cbPNe4">     
+                            </iframe>
+                        </div>
+                    </div>
+                </div>
+                <hr/>
+                <div class="row">
+                    <div class="col-md-12">
+                        <h4 class="text-justify">Kaka and Marta are 10's shirt and feel at Orlando City</h4>
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/1Ygm2-55S-A">     
+                            </iframe>
+                        </div>
+                    </div>
+                </div>
+                <hr/>
+                <div class="row">
+                    <div class="col-md-12">
+                        <h4 class="text-justify">Kaká welcome to Orlando</h4>
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zl0QlzsPcEE">     
+                            </iframe>
+                        </div>
+                    </div>
+                </div>
+                <hr/>
+                <div class="row">
+                    <div class="col-md-12">
+                        <h4 class="text-justify">Orlando City Stadium Tour</h4>
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/N80_T-yz7ak">     
+                            </iframe>
+                        </div>
+                    </div>
+                </div>
+                <hr/>
+            </div>
+            <div id="publicidade" class="col-md-4">
+                <div class="row">
+                    <div class="col-md-12">
+                        <img id="cartao" class="img-responsive" src="img/publicidades/cartao.png">
+                        </div>
+                    </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <img id="cartao" class="img-responsive" src="img/publicidades/app.png">
+                        </div>
+                    </div>
+                <div class="row">
+                    <div id="anuncio1" class="col-md-12">
+                        <h3>Earn Cash Back and LionNation Points With Your FAIRWINDS Credit Card</h3>
+                        </div>
+                    </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <img class="img-responsive center-block" src="img/publicidades/torcida-brasil.png">
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+</section> <!-- Fim - Notícias -->
+<!-- Estatísticas -->
+<section id="scores" class="text-center">
+    <div class="jumbotron container-fluid">
+        <div class="row">
+        <div class="col-md-4">
+            <h1>61348</h1>
+            <h3>SATADIUN CAPACITY</h3>
+        </div>
+        <div class="col-md-4">
+            <h1>2010</h1>
+            <h3>FOUNDED</h3>
+        </div>
+        <div class="col-md-4">
+            <h1>7TH</h1>
+            <h3>HEASTERN CONFERENCE</h3>
+        </div>
+        </div>
+    </div>
+</section> <!-- Fim - Estatísticas -->
+<!-- Acesso a Loja -->
+<section id="call-to-action">
+    <div class="jumbotron container-fluid">
+        <div class="row">
+            <h1 class="text-center">American Club Number One in Brasil</h1>
+            <h2 class="text-center">Visit our store</h2>
+            <div class="container text-center">
+            <div class="row botoes">
+                <div class="col-xs-6">
+                    <a href="#" class="btn botao-roxo hvr-float progress-line-button">Shop</a>
+                </div>
+                <div class="col-xs-6">
+                    <a href="#" class="btn botao-amarelo hvr-float progress-line-button">Register</a>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
+</section> <!-- Fim - Acesso a Loja -->
+<!-- Rodapé -->
+<footer>
+    <div class="container-fluid">
+    <div class="row rodape-1">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2 text-center">
+                    <img class="logo" src="img/logo-orlando.png" alt="logo">
+                </div>
+                <div class="col-md-10">
+                    <div class="row">
+                        <div class="col-md-4 col-popular-posts">
+                            <h4>Popular Posts</h4>
+                            <hr/>
+                            <ul>
+                                <li><a href="#">Orlando City Soccer Stadiun is Open to the Public</a></li>
+                                <time>May 02, 2018</time>
+                                <li><a href="#">Martha is Orlando's newest Contractor</a></li>
+                                <time>May 16, 2018</time>
+                            </ul>
+                        </div>
+                        <div class="col-md-4 col-links">
+                            <h4>Links</h4>
+                            <hr/>
+                            <ul class="list-unstyled">
+                                <li><a href="#"><i class="fa fa-angle-right"> Tickets</i></a></li>
+                                <li><a href="#"><i class="fa fa-angle-right"> News</i></a></li>
+                                <li><a href="#"><i class="fa fa-angle-right"> Schedule</i></a></li>
+                            </ul>
+                        </div>
+                        <div class="col-md-4 col-get-in-touch">
+                            <h4>Get in Touch</h4>
+                            <hr/>
+                            <address>
+                                <p><i class="fa fa-map-marker"></i> 618 E. South Street, Suite 510.<br/>Orlando, FL 32801</p>
+                                <p><a href="tel:1855ORLCITY"><i class="fa fa-phone"></i> 1.855.ORL.CITY</a></p>
+                            </address>
+                            <div class="redes-sociais">
+                                <a href="#"><i class="fab fa-facebook"></i></a>
+                                <a href="#"><i class="fab fa-instagram"></i></a>
+                                <a href="#"><i class="fab fa-twitter-square"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <div class="container-fluid">
+    <div class="row rodape-2">
+        <div class="container texto-rodape">
+            <p class="pull-left copy">Copyright &COPY; Orlando City Soccer 2016. All rights reserved.</p>
+            <p class="pull-right developed">Developed by Vinícius Alcântara</p>
+        </div>
+    </div>
+    </div>
+</footer> <!-- Fim - Rodapé -->
+
+
+    <!-- Jquery -->
+    <script src="js/jquery.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
